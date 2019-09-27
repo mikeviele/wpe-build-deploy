857e2f3bf6beed050889c20bc1d8682e2af434d9
Mike Viele <mike@dev-hero.com>
2019-09-27 13:41:28 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -60,7 +60,7 @@ then
 		if grep -q build:production "$build_file_path";
 		then
 			echo "Building project using gulp"
-			gulp build:production
+			gulp build
 		fi
 	else
 	    echo "Initiating NPM Install"
@@ -80,7 +80,7 @@ then
 		    if grep -q build:production "$build_file_path";
 			then
 			echo "Building project using gulp"
-			gulp build:production
+			gulp build
 		    fi
 	    else
 	    	    # Make sure we have a build command within our grunt file
